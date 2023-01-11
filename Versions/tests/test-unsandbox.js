8f6a96f990ca68332474e4dd703745eec9f44fbf
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-01-11 22:16:40 +0100

    Update test-unsandbox.js
    Signed-off-by: inventionpro <109528211+inventionpro@users.noreply.github.com>

--- Versions/tests/test-unsandbox.js
+++ Versions/tests/test-unsandbox.js
@@ -1,29 +1,40 @@
-// Old sandboxed extensions (worker or <iframe> sandbox):
-class MyExtension {
-  getInfo () {
-    return { /* ... */ };
-  }
-}
-Scratch.extensions.register(new MyExtension());
-
-// Old unsandboxed extensions or "plugins":
-class MyExtension {
-  getInfo () {
-    return { /* ... */ };
-  }
-}
-(function() {
-  var extensionInstance = new MyExtension(window.vm.extensionManager.runtime)
-  var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
-  window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
-})();
-
 // New format for unsandboxed extensions:
 (function(Scratch) {
   'use strict';
   class MyExtension {
-    getInfo () {
-      return { /* ... */ };
+    getInfo() {
+      return {
+        // `id` is the internal ID of the extension
+        // It should never change!
+        // If you choose to make an actual extension, please change this to something else.
+        // Only the characters a-z and 0-9 can be used. No spaces or special characters.
+        id: 'myextensionexample',
+
+        // `name` is what the user sees in the toolbox
+        // It can be changed without breaking projects.
+        name: 'Cool Extension',
+
+        blocks: [
+          {
+            // `opcode` is the internal ID of the block
+            // It should never change!
+            // It corresponds to the class method with the same name.
+            opcode: 'hello',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Hello, world!'
+          }
+        ]
+      };
+    }
+
+    /**
+     * Corresponds to `opcode: 'hello'` above
+     */
+    hello() {
+      // You can just return a value: any string, boolean, or number will work.
+      // If you have to perform an asynchronous action like a request, just return a Promise.
+      // The block will wait until the Promise resolves and return the resolved value.
+      return 'Hello, world!';
     }
   }
   Scratch.extensions.register(new MyExtension());
