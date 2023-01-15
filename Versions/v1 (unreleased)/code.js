39346abc3b186c68280dc8cbad6b885dd1d86c75
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-01-15 12:38:48 +0100

    Update code.js
    Signed-off-by: inventionpro <109528211+inventionpro@users.noreply.github.com>

--- Versions/v1 (unreleased)/code.js
+++ Versions/v1 (unreleased)/code.js
@@ -14,18 +14,24 @@
             // `opcode` is the internal ID of the block. It should never change!
             opcode: 'd4s_base_token',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'Connect to discord, with token'
+            text: 'Connect to discord, with token [TOKEN]'
+            arguments: {
+              TOKEN: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'token'
+              }
+            }
           }
         ]
       };
     }
     
     // Where the blocks do stuff
-    d4s_base_token() {
+    d4s_base_token(args) {
       // You can just return a value: any string, boolean, or number will work.
       // If you have to perform an asynchronous action like a request, just return a Promise.
       // The block will wait until the Promise resolves and return the resolved value.
-      return '';
+      return args.TOKEN;
     }
   }
   Scratch.extensions.register(new D4S());
