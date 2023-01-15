4cc1c8d4db5fcd08a6c9a5c6826030c87606e5d8
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-01-15 12:27:21 +0100

    Update code.js
    Signed-off-by: inventionpro <109528211+inventionpro@users.noreply.github.com>

--- Versions/v1 (unreleased)/code.js
+++ Versions/v1 (unreleased)/code.js
@@ -4,37 +4,28 @@
   class D4S {
     getInfo() {
       return {
-        // `id` is the internal ID of the extension
-        // It should never change!
-        // If you choose to make an actual extension, please change this to something else.
-        // Only the characters a-z and 0-9 can be used. No spaces or special characters.
-        id: 'myextensionexample',
-
-        // `name` is what the user sees in the toolbox
-        // It can be changed without breaking projects.
-        name: 'Cool Extension',
+        // `id` is the internal ID of the extension. It should never change!
+        // `name` is what the user sees in the toolbox. It can be changed
+        id: 'd4s',
+        name: 'D4S',
 
         blocks: [
           {
-            // `opcode` is the internal ID of the block
-            // It should never change!
-            // It corresponds to the class method with the same name.
-            opcode: 'hello',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Hello, world!'
+            // `opcode` is the internal ID of the block. It should never change!
+            opcode: 'd4s_base_token',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Connect to discord, with token'
           }
         ]
       };
     }
-
-    /**
-     * Corresponds to `opcode: 'hello'` above
-     */
-    hello() {
+    
+    // Where the blocks do stuff
+    d4s_base_token() {
       // You can just return a value: any string, boolean, or number will work.
       // If you have to perform an asynchronous action like a request, just return a Promise.
       // The block will wait until the Promise resolves and return the resolved value.
-      return 'Hello, world!';
+      return '';
     }
   }
   Scratch.extensions.register(new D4S());
